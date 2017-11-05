b6ebe713e0df4272261fa44fa9b832467b6bc443
AoHRuthless <kumara0398@gmail.com>
2017-11-05 15:28:54 -0500

    Atom semantic checking function

--- src/parser/prim/ListFuncs.java
+++ src/parser/prim/ListFuncs.java
@@ -5,6 +5,7 @@ package parser.prim;
 
 import parser.Node;
 import parser.SExpression;
+import parser.util.Pat;
 
 /**
  * @author Anand
@@ -13,12 +14,16 @@ import parser.SExpression;
 public class ListFuncs implements PrimitiveMarker {
 
 	@Primitive(aliases = { "car", "first" })
-	public static Node car(SExpression s) {
-		return s.getAddr();
+	public static Node car(SExpression sexp) {
+		return sexp.getAddr();
 	}
 
 	@Primitive(aliases = { "cdr", "rest" })
-	public static Node cdr(SExpression s) {
-		return s.getData();
+	public static Node cdr(SExpression sexp) {
+		return sexp.getData();
+	}
+	
+	public static Node atom(SExpression sexp) {
+		return Node.makeNode(Pat.LITERAL.matches(sexp.getAddr().eval().toString()));
 	}
 }
