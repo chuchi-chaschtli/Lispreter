d6e9c24d36a0cef3c090f89cae595083c9d7ad6a
AoHRuthless <kumara0398@gmail.com>
2017-11-11 18:11:16 -0500

    Refactoring/Documentation

--- src/interpreter/parser/Parser.java
+++ src/interpreter/parser/Parser.java
@@ -10,45 +10,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Representation for the Parser of the Interpreter. While the Lexer tokenizes a
+ * program string into words, the Parser analyzes the grammar of the tokens and
+ * evaluates the tokens accordingly. The output location for evaluation is
+ * immutable.
+ * 
  * @author Anand
  *
  */
 public class Parser {
 
 	private List<Tree> statements;
+	private final Appendable output;
 
+	/**
+	 * Default constructor requires a collection of tokens, and passes the
+	 * System console as the print stream.
+	 * 
+	 * @param tokens
+	 *            a List of program tokens.
+	 * @see #Parser(List, Appendable)
+	 */
 	public Parser(List<String> tokens) {
-		statements = new ArrayList<>();
+		this(tokens, System.out);
+	}
+
+	/**
+	 * Constructor requires a collection of tokens and an output evaluation
+	 * location. The tokens passed are analyzed and used to construct a Tree of
+	 * parse statements.
+	 * 
+	 * @param tokens
+	 *            a List of program tokens.
+	 * @param output
+	 *            a valid Appendable instance.
+	 */
+	public Parser(List<String> tokens, Appendable output) {
+		this.statements = new ArrayList<>();
+		this.output = output;
 		List<String> tmp;
 		int i = 0, j = 0, k = 0;
 		while (i < tokens.size() && i > -1) {
-			j = ListUtils.findFirstOpenParen(ListUtils.subList(tokens, i , true)) + i;
+			j = ListUtils
+					.findFirstOpenParen(ListUtils.subList(tokens, i, true)) + i;
 			if (j == i) {
-				k = ListUtils.clauseEnd(ListUtils.subList(tokens, i, true)) + i + 1;
+				k = ListUtils.clauseEnd(ListUtils.subList(tokens, i, true)) + i
+						+ 1;
 			} else if (j > i) {
 				k = j;
 			} else {
 				k = tokens.size();
 			}
-			tmp = ListUtils.inDotNotation(ListUtils.subList(tokens, i, k,
-					true));
+			tmp = ListUtils
+					.inDotNotation(ListUtils.subList(tokens, i, k, true));
 			statements.add(new Tree(tmp));
 			i = k;
 		}
 	}
 
+	/**
+	 * Evaluates the parsing tree and sends the output to {@code output}.
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs from the Appendable object.
+	 */
 	public void eval() {
 		try {
-			eval(System.out);
+			for (Tree t : statements) {
+				output.append(t.eval()).append("\n");
+			}
 		}
 		catch (IOException e) {
 			e.printStackTrace();
-		}
-	}
-
-	public void eval(Appendable out) throws IOException {
-		for (Tree t : statements) {
-			out.append(t.eval()).append("\n");
 		}
 	}
 }
