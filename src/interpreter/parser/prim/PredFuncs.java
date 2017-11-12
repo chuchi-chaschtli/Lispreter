0ab809cebc1ed7dd8ac46a56cfa14c9bb6e9b63f
AoHRuthless <kumara0398@gmail.com>
2017-11-11 19:42:00 -0500

    Fix predicate evaluation.
    Squash bugs with and, or, not operations.
    And -> Logical and of ALL arguments
    Or -> Logical or of ALL arguments
    Not -> Logical not of the FIRST argument.

--- src/interpreter/parser/prim/PredFuncs.java
+++ src/interpreter/parser/prim/PredFuncs.java
@@ -4,44 +4,78 @@
 package interpreter.parser.prim;
 
 import interpreter.exception.FuncDefException;
+import interpreter.exception.NodeInitException;
 import interpreter.parser.Node;
 import interpreter.parser.SExpression;
 
 /**
+ * Basic predicate functions: Logical AND, NOT, OR.
+ * 
  * @author Anand
  *
  */
 public class PredFuncs implements PrimitiveMarker {
 
+	/**
+	 * Computes the logical AND of all provided arguments.
+	 * 
+	 * @param sexp
+	 *            the given S-Expression.
+	 * @return T if all arguments evaluate to T, NIL otherwise.
+	 */
 	@Primitive(aliases = "and")
 	public static Node and(SExpression sexp) {
 		if (sexp.getAddr().eval(true).toString().equals("NIL")) {
 			return BoolFuncs.nil();
 		}
-		if (sexp.getDataTokens().isEmpty()) {
+		try {
+			return and(new SExpression(sexp.getData()));
+		}
+		catch (NodeInitException e) {
 			return BoolFuncs.t();
 		}
-		return and(new SExpression(sexp.getData()));
 	}
 
+	/**
+	 * Computes the logical OR of all provided arguments.
+	 * 
+	 * @param sexp
+	 *            the given S-Expression.
+	 * @return T if any argument evaluate to T, NIL otherwise.
+	 */
 	@Primitive(aliases = "or")
 	public static Node or(SExpression sexp) {
 		if (sexp.getAddr().eval(true).toString().equals("T")) {
 			return BoolFuncs.t();
 		}
-		if (sexp.getDataTokens().isEmpty()) {
+		try {
+			return or(new SExpression(sexp.getData()));
+		}
+		catch (NodeInitException e) {
 			return BoolFuncs.nil();
 		}
-		return and(new SExpression(sexp.getData()));
 	}
 
+	/**
+	 * Computes the logical NOT of the first argument.
+	 * 
+	 * @param sexp
+	 *            the given S-Expression.
+	 * @return T if the first argument evaluates to NIL, NIL if the first
+	 *         argument evaluates to T.
+	 * @throws FuncDefException
+	 *             if the first argument is neither T nor NIL.
+	 */
 	@Primitive(aliases = "not")
 	public static Node not(SExpression sexp) {
 		String evaluation = sexp.getAddr().eval(true).toString();
-		Node n = BoolFuncs.boolFuncFactory(evaluation);
-		if (n == null) {
+		if (evaluation.equals("T")) {
+			evaluation = "NIL";
+		} else if (evaluation.equals("NIL")) {
+			evaluation = "T";
+		} else {
 			throw new FuncDefException("Cannot negate a non-boolean function");
 		}
-		return n;
+		return BoolFuncs.boolFuncFactory(evaluation);
 	}
 }
